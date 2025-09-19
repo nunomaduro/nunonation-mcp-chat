91ec413a62bd967e9897d6cc1eab86254faf1eb7
Nuno Maduro <enunomaduro@gmail.com>
2025-09-19 17:52:23 +0100

    fix: variable name

--- tests/Unit/Models/MessageTest.php
+++ tests/Unit/Models/MessageTest.php
@@ -5,9 +5,9 @@ declare(strict_types=1);
 use App\Models\Message;
 
 test('to array', function () {
-    $user = Message::factory()->create()->refresh();
+    $message = Message::factory()->create()->refresh();
 
-    expect(array_keys($user->toArray()))
+    expect(array_keys($message->toArray()))
         ->toBe([
             'id',
             'name',
